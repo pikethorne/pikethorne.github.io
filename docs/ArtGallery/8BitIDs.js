2586fe5534e9926bfe901d0f2cadeb770023424d
Madelyn Machado <plummachado97@gmail.com>
2024-02-22 01:35:27 -0600

    Added drive loading prefixes and sample 8 art

---
+++ docs/ArtGallery/8BitIDs.js
@@ -0,0 +1,8 @@
+// there's probably a really easy way to do this but i'm a lil crazy and i don't know shit about web development
+
+var EightBitIDs = [
+'1I_AFsIG2rdUZmPu0yCoZZVvTPPVvgmQC', // Force of Negation
+'1H7FytbWr5-foKVYwRxSSY_OKHLa5DkX6', // Sol Ring
+'19dYZWPzw_igZgoDiRiUd6m7TL_2zYnCX', // Worldslayer
+'1TURRPoH532NDMTUavM9WU-OyC0VuX8nt' // Blade of Selves
+];
---
+++ docs/ArtGallery/DrivePrefix.js
@@ -0,0 +1,1 @@
+var GDPrefix = 'https://drive.google.com/thumbnail?id=';
