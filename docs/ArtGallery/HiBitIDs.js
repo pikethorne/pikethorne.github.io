6407f2b702574b93bd69f45ae928450dedf91d25
Madelyn Machado <plummachado97@gmail.com>
2024-02-22 19:19:46 -0600

    Added additional card galleries for hi bit and and other cards

---
+++ docs/ArtGallery/HiBitIDs.js
@@ -0,0 +1,11 @@
+// i need to find a proper image host so i don't use up my 1 singular gigabyte
+
+var HiBitIDs = [
+'galina', // 
+'greased', // 
+'inked', // 
+'svyelun', // 
+'copy', // 
+'insect', // 
+'secondsun' // 
+];
---
+++ docs/ArtGallery/OtherIDs.js
@@ -0,0 +1,6 @@
+// i need to find a proper image host so i don't use up my 1 singular gigabyte
+
+var OtherIDs = [
+'MoonMagus', // 
+'notebookvenser3' // 
+];
